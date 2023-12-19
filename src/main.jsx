6c293fcba92250e49574eab1fe84fde46ccc1c0d
Bedřich Schindler <bedrich.schindler@gmail.com>
2023-12-19 14:56:44 +0100

    Use styles from React UI

--- src/main.jsx
+++ src/main.jsx
@@ -4,7 +4,10 @@ import { IndexPage } from './pages/index';
 
 // React UI core CSS
 import '@react-ui-org/react-ui/src/lib/theme.scss';
-import '@react-ui-org/react-ui/src/lib/foundation.scss';
+import '@react-ui-org/react-ui/src/lib/foundation.scss'; // Comment if you want to use Modal styles only
+
+// Import Modal styles only
+// import '@react-ui-org/react-ui/src/lib/components/Modal/Modal.scss';
 
 // React UI CSS helpers
 import '@react-ui-org/react-ui/src/lib/helpers.scss';
